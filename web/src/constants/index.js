5a7745431ad7da651d6d223b36a0c8ad9a7bf435
stalkingbutler <scinewave@gmail.com>
2016-04-03 15:22:10 -0400

    nginx and api ip edits.

--- web/src/constants/index.js
+++ web/src/constants/index.js
@@ -1,2 +1,17 @@
-export const TWITTER_API_URL = 'http://127.0.0.1:4040';
+export const TWITTER_API_URL = 'http://api:4000';
+// export const TWITTER_API_URL = '172.17.0.3:4000';
+// export const TWITTER_API_URL = 'http://10.0.2.15:4000';
+// export const TWITTER_API_URL = 'http://172.17.0.1:4000';
+// export const TWITTER_API_URL = 'localhost:4000';
+// export const TWITTER_API_URL = 'http://127.0.0.1:4000';
+// export const TWITTER_API_URL = 'http://0.0.0.0:4000';
+// export const TWITTER_API_URL = 'http://api:4000';
+// export const TWITTER_API_URL = 'http://172.17.0.3:4000';
+//export const TWITTER_API_URL = 'http://172.17.0.3:4000';
+// export const TWITTER_API_URL = 'http://0.0.0.0:4000';
+// export const TWITTER_API_URL = 'http://127.0.0.1:4040';
+// API_1_PORT_4000_TCP_PORT
+// export const TWITTER_API_URL = `http://${process.env.API_1_PORT_4000_TCP_ADDR}:4000`;
 export const PER_PAGE = 12;
+
+// SUMIT_API_1_PORT=tcp://172.17.0.3:4000
