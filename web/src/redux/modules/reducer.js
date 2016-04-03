13c6261c01c3a7bc7505de8fa1803a20a38afe25
stalkingbutler <scinewave@gmail.com>
2016-04-03 11:23:05 -0400

    Clean up.

--- web/src/redux/modules/reducer.js
+++ web/src/redux/modules/reducer.js
@@ -1,24 +1,15 @@
 import { combineReducers } from 'redux';
-import multireducer from 'multireducer';
 import { routeReducer } from 'react-router-redux';
 import {reducer as reduxAsyncConnect} from 'redux-async-connect';
 
 import clicks from './clicks';
-import counter from './counter';
-import info from './info';
 import users from './users';
 import query from './query';
 
 export default combineReducers({
   routing: routeReducer,
   reduxAsyncConnect,
-  multireducer: multireducer({
-    counter1: counter,
-    counter2: counter,
-    counter3: counter
-  }),
   clicks,
-  info,
   query,
   users
 });
