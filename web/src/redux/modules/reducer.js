6a815ac749d76c4226cc97370323ea930fb05576
stalkingbutler <scinewave@gmail.com>
2016-04-24 08:15:56 -0400

    Clean up.

--- web/src/redux/modules/reducer.js
+++ web/src/redux/modules/reducer.js
@@ -2,14 +2,10 @@ import { combineReducers } from 'redux';
 import { routeReducer } from 'react-router-redux';
 import {reducer as reduxAsyncConnect} from 'redux-async-connect';
 
-import clicks from './clicks';
 import requests from './requests';
-import query from './query';
 
 export default combineReducers({
   routing: routeReducer,
   reduxAsyncConnect,
-  clicks,
-  query,
   requests
 });
